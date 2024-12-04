8261fa4aa47ad948126a54a298b29eabc4fe66e2
Andres Osorio <af.osorio1341@gmail.com>
2024-12-04 09:33:43 +0100

    add copy icon to code blocks

--- public/js/main.js
+++ public/js/main.js
@@ -10,4 +10,43 @@ document.addEventListener("DOMContentLoaded", function() {
             flash.classList.add("hide");
         }, FLASH_DELAY + i * 1000);
     });
+
+    const codeBlocks = document.querySelectorAll("pre");
+
+    if (codeBlocks.length) {
+        codeBlocks.forEach(block => {
+            const copyIconHTML = '<i class="fa-regular fa-copy"></i>';
+            const copyIconSuccessHTML = '<i class="fa-solid fa-check"></i>'
+            const copyIconWrapper = document.createElement('span');
+            const blockText = block.querySelector('code')?.innerText || '';
+
+            copyIconWrapper.classList.add('copy-code-btn');
+            copyIconWrapper.dataset.tooltip = 'Copy';
+            copyIconWrapper.innerHTML = copyIconHTML;
+            copyIconWrapper.addEventListener('click', () => {
+                copyIconWrapper.innerHTML = copyIconSuccessHTML;
+
+                copyToClipboard(blockText)
+
+                setTimeout(() => {
+                    copyIconWrapper.innerHTML = copyIconHTML;
+                }, 1000)
+            })
+
+            block.appendChild(copyIconWrapper);
+        })
+    }
+
+    /**
+     * Copies the provided text to the clipboard
+     * @param {string} textToCopy
+     * @returns void
+     */
+    async function copyToClipboard(textToCopy) {
+        try {
+            await navigator.clipboard.writeText(textToCopy);
+        } catch (e) {
+            console.log('Failed to copy!', e);
+        }
+    }
 });
